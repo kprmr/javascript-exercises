cb9e0b3c6cc79068b32b4a69d8a2f471296a5ddd
kprmr <parmarskush@gmail.com>
2025-08-31 23:30:03 +0530

    Added sumAll function

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    let sum = 0;
+    if (x < 0 || y < 0 || !Number.isInteger(x) || !Number.isInteger(y)) return "ERROR"
+    if (x < y) {
+        for (x; x <= y; x++){
+            sum = sum + x;
+        }
+    }
+    else if (x > y) {
+        for (y; y <= x; y++){
+            sum = sum + y;
+        }
+  }
 
+    return sum
 };
 
 // Do not edit below this line
