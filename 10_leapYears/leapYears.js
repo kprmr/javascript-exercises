e73ddbaf3142b4f01864c4626274bdb54d4923ff
kprmr <parmarskush@gmail.com>
2025-09-01 00:17:09 +0530

    Removed if and else if statement instead directly returned boolean value

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,8 +1,5 @@
 const leapYears = function(year) {
-    if (year % 400 === 0) return true;
-    else if (year % 100 === 0) return false;
-    else if (year % 4 === 0) return true;
-    else return false
+    return (year % 400 === 0) || ((year % 4 === 0) && (year % 100 !== 0))
 };
 
 // Do not edit below this line
