7e130e8cece5396a35089f2257268cb1c4f5e484
kprmr <parmarskush@gmail.com>
2025-09-01 00:13:27 +0530

    Removed the redundance of nested if instead used if else if

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,14 +1,8 @@
 const leapYears = function(year) {
-    if(year % 4 == 0) {
-        if (year % 100 != 0) {
-            return true
-        }
-        else if (year % 400 == 0) {
-            return true
-        }
-        else return false
-    }
-    else return false;
+    if (year % 400 === 0) return true;
+    else if (year % 100 === 0) return false;
+    else if (year % 4 === 0) return true;
+    else return false
 };
 
 // Do not edit below this line
