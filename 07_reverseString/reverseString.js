f87be07877ea938b6d08477aa5c44087b993ac7e
kprmr <parmarskush@gmail.com>
2025-08-31 18:13:15 +0530

    Added reverseString function

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(line) {
+    let arr = []
+    arr = line.split('')
+    let newArr = arr.reverse()
+    let newLine = newArr.join('')
+    return newLine
 };
 
 // Do not edit below this line
