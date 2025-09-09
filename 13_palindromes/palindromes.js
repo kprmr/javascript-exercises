363dd8cd365e2fb9c5bbee0c84a4fb70cd8e374f
Kushagra Parmar <parmarskush@gmail.com>
2025-09-09 23:56:19 +0530

    did as directed

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let str1 = str.replaceAll(" ", "").toLowerCase().replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, '');
+    let rev = str1.split('').reverse().join('')  ;
+    if (rev === str1) {
+        return true
+    }
+    else return false
 };
 
 // Do not edit below this line
