94db3dde056ee353f8ec253d48952657243802be
Kushagra Parmar <parmarskush@gmail.com>
2025-09-09 23:00:41 +0530

    made function to do thing in calculator.js deleted .skips from calculator.spec.js

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,58 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1+num2
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1-num2
 };
 
-const sum = function() {
-	
+const sum = function(...args) {
+  let sum = 0;
+  if (Array.isArray(args[0])){
+    let a = args[0];
+    if (a.length === 0) {
+      return 0;
+    }
+    const newArr = a.map(Number);
+    for (let num of newArr) {
+      sum = sum + num;
+    }
+  }
+  else {
+    for (let numb of args){
+      sum = sum + numb;
+    }
+  }
+  return sum;
 };
 
-const multiply = function() {
 
+
+const multiply = function(...args) {
+  let product = 1;
+  let a = args[0]
+  const newArr = a.map(Number);
+  for (let num of newArr) {
+    product = product * num;
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(x, n) {
+  let result = 1;
+	for (let i = 0; i< n; i++) {
+    result = result * x
+  }
+  return result
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let fact = 1;
+  if (num === 0) return fact;
+  for (num; num > 0; num--) {
+    fact = fact * num
+  }
+  return fact;
 };
 
 // Do not edit below this line
