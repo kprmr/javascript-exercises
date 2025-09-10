9c5add679af6504a5ce6372fe681548e803a7f2d
Kushagra Parmar <parmarskush@gmail.com>
2025-09-10 08:06:31 +0530

    Made the changes in fibonacci function where it return the specif number in fibonacci series in fibonacci.js and deleted .skips in fibonacci.spec.js

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
 
+    n = Number(n)
+    let num1 = 1;
+    let num2 = 1;
+    let fib = 0;
+    let fibArr = [];
+    fibArr.push(num1);
+    fibArr.push(num2);
+    if (n === 0) return fib;
+    if (n < 0) return "OOPS"
+    for (let i = 0; i < n; i++) {
+        feb = num1 + num2;
+        fibArr.push(feb);
+        num1 = num2;
+        num2 = feb;
+    }
+    return fibArr[n-1];
 };
 
 // Do not edit below this line
